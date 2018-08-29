2fce272e66463aca9b28b083c8f5653da2d42743
Yanyan Hu <yanyanhu@sg.ibm.com>
2018-08-29 05:46:23 +0000

    Fix a minor error in fabric.sh

--- first-network/fabric.sh
+++ first-network/fabric.sh
@@ -97,7 +97,7 @@ function clearContainers () {
 # specifically the following images are often left behind:
 # TODO list generated image naming patterns
 function removeUnwantedImages() {
-  DOCKER_IMAGE_IDS=$(docker images | grep "dev\|none\|test-vp\|peer[0-9]-" | awk '{print $3}')
+  DOCKER_IMAGE_IDS=$(docker images | grep "dev-peer\|none\|test-vp\|peer[0-9]-" | awk '{print $3}')
   if [ -z "$DOCKER_IMAGE_IDS" -o "$DOCKER_IMAGE_IDS" == " " ]; then
     echo "---- No images available for deletion ----"
   else
